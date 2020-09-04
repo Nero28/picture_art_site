5f7706e59c06b5d7e0471c7543a00e1e4c410e51
Nero28 <shackiy.s@yandex.ru>
2020-09-04 09:54:25 +0300

    button for download more elements

--- src/js/main.js
+++ src/js/main.js
@@ -3,16 +3,18 @@ import sliders from './modules/sliders';
 import forms from './modules/forms';
 import mask from './modules/mask';
 import checkTextInputs from './modules/checkTextInputs';
+import showMoreStyles from './modules/showMoreStyles';
 
 
 window.addEventListener('DOMContentLoaded', () => {
-'use strict';
+    'use strict';
 
-modals();
-sliders('.feedback-slider-item','horizontal','.main-prev-btn','.main-next-btn');
-sliders('.main-slider-item','vertical');
-forms();
-mask('[name="phone"]');
-checkTextInputs('[name="name"]');
-checkTextInputs('[name="message"]');
+    modals();
+    sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
+    sliders('.main-slider-item', 'vertical');
+    forms();
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
+    showMoreStyles('.button-styles', '.styles-2');
 })
